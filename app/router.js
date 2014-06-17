792b8cbe989639467d1349d90fedf786eb633b35
Fredrik Mundt <famthegeek95@gmail.com>
2014-06-17 14:22:35 +0200

    Fixed the footer and removed some minor tests

--- app/router.js
+++ app/router.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 var Router = Ember.Router.extend({
-  location: UngepiraterDOTnoENV.locationType
+	location: UngepiraterDOTnoENV.locationType
 });
 
 Router.map(function() {
